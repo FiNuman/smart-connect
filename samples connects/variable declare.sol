6d9946121d2f9fd981c05ccf25858488c8929969
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2023-09-05 23:36:40 +0600

    Update variable declare.sol

--- samples connects/variable declare.sol
+++ samples connects/variable declare.sol
@@ -1,1 +1,11 @@
+ 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ownerable{
+    bool public booldata = true;
+    int32 public int32data = 123;
+    uint public uintdata = 100;
+    address public addressdata=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes32 public butes32data;
+}
