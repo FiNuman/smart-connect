d7890bd45055d5fb3138259d99b85e3793788c1b
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2023-09-05 14:11:40 +0600

    Update sample 1.sol
    basic contacct for understand how it work

--- samples connects/sample 1.sol
+++ samples connects/sample 1.sol
@@ -1,1 +1,27 @@
 
+ 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ownerable{
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+    function setOwner(address _newowner) public onlyOwner(){
+        require(_newowner != address(0),"invalid address");
+        owner = _newowner;
+    }
+
+    function anyone() public {
+        //code
+    }
+}
